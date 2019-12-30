62325fb77e5bcaa6f85cf2494c277fa97a510ffa
DhanooshTamizh <54681831+DhanooshTamizh@users.noreply.github.com>
2019-12-30 22:21:04 +0530

    Update face_recog_full.py

--- face_recog_full.py
+++ face_recog_full.py
@@ -4,8 +4,7 @@ import pickle
 import os
 
 face_cascade = cv2.CascadeClassifier('C:/ProgramData/Anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:/ProgramData/Anaconda3/Lib/site-packages/cv2/data/haarcascade_eye.xml')
-#smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+
 
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -19,7 +18,7 @@ with open("face-labels.pickle", 'rb') as f:
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame
+   
     ret, frame = cap.read()
     gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
@@ -28,11 +27,10 @@ while(True):
     	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
     	roi_color = frame[y:y+h, x:x+w]
 
-    	# recognize? deep learned model predict keras tensorflow pytorch scikit learn
+    	
     	id_, conf = recognizer.predict(roi_gray)
     	if conf>=4 and conf <= 85:
-    		#print(5: #id_)
-    		#print(labels[id_])
+    		
     		font = cv2.FONT_HERSHEY_SIMPLEX
     		name = labels[id_]
     		color = (255, 255, 255)
@@ -47,14 +45,11 @@ while(True):
     	end_cord_x = x + w
     	end_cord_y = y + h
     	cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
-    	#subitems = smile_cascade.detectMultiScale(roi_gray)
-    	#for (ex,ey,ew,eh) in subitems:
-    	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    # Display the resulting frame
+  
     cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+
 cap.release()
 cv2.destroyAllWindows()
